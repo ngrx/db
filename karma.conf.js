84398d6fb977d858bdfb94303121d5806f547cdb
Mike Ryan <mike.ryan52@gmail.com>
2016-09-06 07:47:13 -0500

    chore: Rework build and upgrade to Angular 2 RC6 (#8)

--- karma.conf.js
+++ karma.conf.js
@@ -1,80 +1,82 @@
-// Karma configuration
-// Generated on Wed Jan 06 2016 19:58:14 GMT-0800 (PST)
-var argv = require('yargs').argv;
+var path = require('path');
 
-module.exports = function(config) {
-  config.set({
+module.exports = function(karma) {
+  'use strict';
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+  karma.set({
+    basePath: __dirname,
 
-    plugins: [ require('./spec/support/tsc-preprocessor'), 'karma-jasmine', 'karma-chrome-launcher'],
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
-    // list of files / patterns to load in the browser
     files: [
-      'node_modules/es6-shim/es6-shim.js',
-      'node_modules/systemjs/dist/system.js',
-      'spec/support/karma-system.js',
-      'node_modules/reflect-metadata/Reflect.js',
-      {pattern: 'node_modules/@angular/**/*.js', included: false},
-      {pattern: 'node_modules/rxjs/**/*.js', included: false},
-
-      {pattern: 'src/**/*', included: false},
-      {pattern: 'spec/**/*', included: false}
+      { pattern: 'tests.bundle.ts', watched: false }
     ],
 
+    exclude: [],
 
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'spec/*.ts': ['tsc'],
-      'src/*.ts': ['tsc']
+      'tests.bundle.ts': ['coverage', 'webpack', 'sourcemap']
     },
 
+    reporters: ['mocha', 'coverage'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    coverageReporter: {
+      dir: 'coverage/',
+      reporters: [
+        { type: 'text-summary' },
+        { type: 'json' },
+        { type: 'html' }
+      ]
+    },
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+    port: 9018,
+    runnerPort: 9101,
+    colors: true,
+    logLevel: karma.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    webpackServer: { noInfo: true },
+    webpack: {
+      devtool: 'inline-source-map',
+      resolve: {
+        root: __dirname,
+        extensions: ['', '.ts', '.js']
+      },
+      module: {
+        preLoaders: [
+          {
+            test: /\.ts$/,
+            loader: 'tslint-loader',
+            exclude: [
+              /node_modules/
+            ]
+          }
+        ],
+        loaders: [
+          {
+            test: /\.ts?$/,
+            exclude: /(node_modules)/,
+            loader: 'ts'
+          }
+        ],
+        postLoaders: [
+          {
+            test: /\.(js|ts)$/, loader: 'istanbul-instrumenter',
+            include: path.resolve(__dirname, 'src'),
+            exclude: [
+              /\.(e2e|spec|bundle)\.ts$/,
+              /node_modules/
+            ]
+          }
+        ]
+      },
+      tslint: {
+        emitErrors: false,
+        failOnHint: false,
+        resourcePath: 'src'
+      }
+    }
+  });
+};
